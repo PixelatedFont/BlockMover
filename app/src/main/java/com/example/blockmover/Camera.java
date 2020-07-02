8933f8b6ac9e5bb964b24aac2325eef2b4dda80b
PixelatedFont <Hexedtitan@gmail.com>
2020-07-02 13:33:15 +0700

    test

---
+++ app/src/main/java/com/example/blockmover/Camera.java
@@ -0,0 +1,4 @@
+package com.example.blockmover;
+
+public class Camera {
+}
---
+++ app/src/main/java/com/example/blockmover/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.blockmover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Disable Title Bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Set Screen to Landscape Mode
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //Set View
+        setContentView(new GameView(this));
+
+    }
+
+}
