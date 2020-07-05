7ffb31c663b7e495df14dae8d88504e51b582b16
PixelatedFont <Hexedtitan@gmail.com>
2020-07-05 15:16:44 +0700

    Scene Management Implemented

--- app/src/main/java/com/example/blockmover/GameView.java
+++ app/src/main/java/com/example/blockmover/GameView.java
@@ -15,13 +15,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 {
     //This class represent the view on the screen
     private MainThread thread;
-    private PlayerObject player;
-    private InGameObject gameObject;
-    private Point playerPoint;
-    private Point gameObjectPoint;
-    private InGameObject[][] gameObjects;
-    private Point[][] points;
-
+    private SceneManager manager;
 
     int pointX = 1000;
     int pointY = 300;
@@ -36,36 +30,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
         getHolder().addCallback(this);
 
         thread = new MainThread(getHolder(),this);
-        //Create player object here
-        player = new PlayerObject(new Rect(100,100,200,200), Color.rgb(100,0,0));
 
-        playerPoint = new Point(600,600);
-
-        //Create game object here
-        gameObject = new InGameObject(new Rect(100,100,200,200), Color.rgb(0,100,0), true);
-        gameObjectPoint = new Point(800,600);
-
-        gameObjects = new InGameObject[10][10];
-        points = new Point[10][10];
-
-        for (int i = 0; i < 10; i++ )
-        {
-            for (int j = 0; j < 10;j++)
-            {
-                gameObjects[i][j] = new InGameObject(new Rect(100,100,200,200), Color.rgb(100 + i+10,100+j,100), false);
-            }
-        }
-
-        for (int i = 0; i < 10; i++ )
-        {
-            for (int j = 0; j < 10;j++)
-            {
-                System.out.println("Drawing: " + i +" " + j);
-                points[i][j] = new Point(pointX,pointY);
-            }
-        }
-
-
+        manager = new SceneManager();
 
 
         setFocusable(true);
@@ -110,51 +76,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
-        switch (event.getAction())
-        {
-
-        }
+        manager.receiveTouch(event);
         return true;
         //return super.onTouchEvent(event);
     }
 
     public void update()
     {
-        //Handle on screen logic here
-        player.update(playerPoint);
-        gameObject.update(gameObjectPoint);
-        //System.out.println(Constants.xVelocity);
-        //System.out.println(Constants.yVelocity);
 
-
-        //System.out.println(gameObject.playerCollide(player));
-        if (gameObject.playerCollide(player).equals("left"))
-        {
-            System.out.println(gameObject.playerCollide(player));
-            gameObjectPoint.x -= 100;
-        }
-
-        if (gameObject.playerCollide(player).equals("right"))
-        {
-            System.out.println(gameObject.playerCollide(player));
-            gameObjectPoint.x += 100;
-        }
-
-       if (gameObject.playerCollide(player).equals("top"))
-        {
-            System.out.println(gameObject.playerCollide(player));
-            gameObjectPoint.y -= 100;
-        }
-
-        if (gameObject.playerCollide(player).equals("bottom"))
-        {
-            System.out.println(gameObject.playerCollide(player));
-            gameObjectPoint.y += 100;
-        }
-
-
-
-
+        manager.update();
 
        /* float wy = (player.getRectangle().width() + gameObject.getRectangle().width()) * (playerPoint.x - gameObject.getRectangle().centerY());
         float hx = (player.getRectangle().height() + gameObject.getRectangle().height()) * (player.getRectangle().centerX() - gameObject.getRectangle().centerX());
@@ -190,34 +120,12 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
             }
         }
         */
-
-
-
-
-
-
-
-
     }
 
     @Override
     public void draw(Canvas canvas)
     {
-
-        //Draw here
         super.draw(canvas);
-        if (canvas != null)
-        {
-            canvas.drawColor(Color.WHITE);
-
-            //player.draw(canvas);
-            //gameObject.draw(canvas);
-
-
-
-
-
-
-        }
+        manager.draw(canvas);
     }
 }
